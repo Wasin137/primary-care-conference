002ed9bceeed06fd3c834e88efcf1f6bbee3bfec
Wasin Kampeera <wasin.kamp@gmail.com>
2024-01-06 22:07:33 +0700

    update with "&quot;"

--- src/ConferenceDate.js
+++ src/ConferenceDate.js
@@ -4,7 +4,7 @@ export default function ConferenceDate() {
   return (
         <div className="text-center text-white p-4 bg-gray-700 bg-opacity-80 rounded-lg">
             <p className='text-2xl xl:text-6xl'>21-22 Mar 2024</p>
-            <p className="font-bold text-xl xl:text-5xl xl:mt-5 mb-2">"Quality of Life, Quality of Care"</p>
+            <p className="font-bold text-xl xl:text-5xl xl:mt-5 mb-2">&quot;Quality of Life, Quality of Care&quot;</p>
         </div>
   )
 }
--- src/MainContent.js
+++ src/MainContent.js
@@ -6,7 +6,7 @@ export default function MainContent() {
   return (
     <>
         <div className='p-4 bg-white rounded-lg shadow-md'>
-            <p className='text-black-1000 text-3xl xl:text-4xl 2xl:text-6xl mt-5'>"Quality of Life, Quality  of Care"</p>
+            <p className='text-black-1000 text-3xl xl:text-4xl 2xl:text-6xl mt-5'>&quot;Quality of Life, Quality of Care&quot;</p>
             <p className='text-gray-800 text-2xl xl:text-3xl 2xl:text-4xl mt-2'>Hatyai Primary Care Conference 2024</p>
             <p className='text-gray-800 text-base xl:text-lg mt-5'>Fugiat dolor nostrud cillum commodo ut amet. Occaecat dolore pariatur esse aliquip velit nisi commodo et consectetur anim dolore nostrud officia minim. Qui magna culpa nisi elit consectetur irure. Ad voluptate nisi sunt amet id veniam. Esse Lorem eu cupidatat sit Lorem deserunt commodo. Cillum ad ut eiusmod pariatur.</p>
             <div className='mt-5'>
