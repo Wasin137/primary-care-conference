05df5eb5095f1a38b045a81e1543033dabbd5b62
Wasin137 <wasin.kamp@gmail.com>
2024-05-02 14:19:19 +0700

    Update Carousel with NEXT/Image

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,26 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
+import BackgroundImg from '/public/pexels-thirdman-7659567_inaxsy_c_scale,w_1400.jpg'
 
 export default function Carousel() {
     return (
       <Image
-        src="/pexels-thirdman-7659567_inaxsy_c_scale,w_1400.jpg"
-        alt=""
+        src={BackgroundImg}
+        alt="Background Image"
         width={1400}
         height={933}
-        layout='responsive'
-        srcSet={`
-          /pexels-thirdman-7659567_inaxsy_c_scale,w_480.jpg 480w,
-          /pexels-thirdman-7659567_inaxsy_c_scale,w_812.jpg 812w,
-          /pexels-thirdman-7659567_inaxsy_c_scale,w_1102.jpg 1102w,
-          /pexels-thirdman-7659567_inaxsy_c_scale,w_1308.jpg 1308w,
-          /pexels-thirdman-7659567_inaxsy_c_scale,w_1400.jpg 1400w
-        `}
-        sizes={`
-          (max-width: 767px) 100vw,
-          (min-width: 768px) and (max-width: 1199px) 60vw,
-          40vw
-        `}
         unoptimized
       />
     );
