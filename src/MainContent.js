729b702da22c12eadce62b308e789d1ff3786eef
Wasin Kampeera <wasin.kamp@gmail.com>
2024-06-11 22:31:52 +0700

    Update with ESLint rules

--- src/MainContent.js
+++ src/MainContent.js
@@ -8,7 +8,7 @@ export default function MainContent() {
             <p className='text-black-1000 text-3xl xl:text-4xl 2xl:text-6xl mt-5'>&quot;Health Horizons: Bridging Global Health with Local Solutions in Primary Care&quot;</p>
             <p className='text-gray-800 text-2xl xl:text-3xl 2xl:text-4xl mt-2'>งานประชุมวิชาการประจำปี 2567 กลุ่มงานเวชกรรมสังคม โรงพยาบาลหาดใหญ่</p>
             <div className='mt-10 md:mx-10 border shadow-md border-gray-800 rounded-md px-4 py-6'>
-              <p className='text-gray-600 font-bold italic text-2xl xl:text-3xl 2xl:text-4xl text-center'>&quot;Thank you for being with us at 'Health Horizons.' We hope you found it as inspiring and informative as we did!&quot;</p>
+              <p className='text-gray-600 font-bold italic text-2xl xl:text-3xl 2xl:text-4xl text-center'>&quot;Thank you for being with us at &quot;Health Horizons.&quot; We hope you found it as inspiring and informative as we did!&quot;</p>
               <p className='text-gray-400 font-bold text-xl text-end mt-3'>Primary Care Conference 2024</p>
               <p className='text-gray-400 italic text-xl text-end'>- Department of Social Medicine, Hatyai Hospital -</p>
             </div>
